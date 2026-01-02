9228c9fceb50542b170731fbff4d6d0fd65b01d5
AnalyticETH <hello@analytic.lol>
2026-01-01 19:16:33 -0500

    building in progress

--- Jellyfin.Plugin.Hue/Plugin.cs
+++ Jellyfin.Plugin.Hue/Plugin.cs
@@ -5,6 +5,7 @@ using MediaBrowser.Common.Configuration;
 using MediaBrowser.Common.Plugins;
 using MediaBrowser.Model.Serialization;
 using MediaBrowser.Model.Plugins;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Jellyfin.Plugin.Hue
 {
@@ -13,6 +14,7 @@ namespace Jellyfin.Plugin.Hue
         public override string Name => "Philips Hue Sync";
         public override Guid Id => Guid.Parse("4e078f02-ec43-473e-85f1-98e86eb9a761");
 
+        [ActivatorUtilitiesConstructor]
         public Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer)
             : base(applicationPaths, xmlSerializer)
         {
